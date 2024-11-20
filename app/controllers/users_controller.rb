4e86d327bc949f85aecb55753b45ed697691da39
Yusuke Kajita <kaji@example.com>
2024-11-20 07:15:32 +0000

    users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,33 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def create
+    c = User.new(my_company: params[:user][:my_company],
+                 my_name: params[:user][:my_name],
+                 pass: params[:user][:pass])
+    c.save
+    redirect_to root_path
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to root_path
+  end
+  
+  def show
+    @users = User.find(params[:id])
+  end
+  
+  def edit
+    @user =User.find(params[:id])
+  end
+  
+  def update
+    c = User.update(my_company: params[:user][:my_company],
+                 my_name: params[:user][:my_name],
+                 pass: params[:user][:pass])
+    c.save
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   
   get 'top/top_page'
+  resources :users, only: [:new]
   
   root 'top#top_page'
 end
