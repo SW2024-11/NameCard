7d157733841ffa43982d71d62b76059b18df28ce
Yusuke Kajita <kaji@example.com>
2024-11-20 08:39:50 +0000

    users_view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   
   get 'top/top_page'
-  resources :users, only: [:new]
   
   root 'top#top_page'
   
