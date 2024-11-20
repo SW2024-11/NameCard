6e6d16ac85359349060bbfed5064cd4a01b7ac99
Yui Nemoto <2211140081j@ed.fuk.kindai.ac.jp>
2024-11-20 06:55:11 +0000

    def(cards_controller)

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,61 @@
 class CardsController < ApplicationController
+  def index
+    @cards = Card.all.order(tdate: :desc)
+  end
+  
+  def new
+    @card = Card.new
+  end
+  
+  def create
+    @card = Card.new(company: params[:card][:company], 
+                    name: params[:card][:name], 
+                    position: params[:card][:position],
+                    license: params[:card][:license],
+                    mail: params[:card][:mail],
+                    tell: params[:card][:tell],
+                    company_adress: params[:card][:company_adress],
+                    image: params[:card][:image])
+    if @card.save
+      flash[:notice] = '追加しました。'
+      redirect_to root_path
+    else
+      render 'new'#, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @card = Card.find(params[:id])
+    if @card.destroy
+      flash[:notice] = '削除しました。'
+      redirect_to root_path
+    else
+      render 'index'#, status: :unprocessable_entity
+    end
+  end
+  
+  def show
+    @card = Card.find(params[:id])
+  end
+  
+  def edit
+    @card = Card.find(params[:id])
+  end
+  
+  def update
+    @card = Card.find(params[:id])
+    if @card.update(company: params[:card][:company], 
+                    name: params[:card][:name], 
+                    position: params[:card][:position],
+                    license: params[:card][:license],
+                    mail: params[:card][:mail],
+                    tell: params[:card][:tell],
+                    company_adress: params[:card][:company_adress],
+                    image: params[:card][:image])
+      flash[:notice] = '更新しました。'
+      redirect_to root_path
+    else
+      render 'edit'#, status: :unprocessable_entity
+    end
+  end
 end
