b39736b572ac6f122152b3b5c5e9f95130e09ca4
Yusuke Kajita <kaji@example.com>
2024-11-26 07:28:55 +0000

    login_top_controller

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -4,4 +4,14 @@ class TopController < ApplicationController
             redirect_to users_path
         end
     end
+    
+    def login
+        user = User.find_by(uid: params[:uid])
+        if  user and BCrypt::Password.new(user.pass) == params[:pass]
+            session[:login_uid] = params[:uid]
+            redirect_to top_main_path
+        else
+            render "error",status:422 
+        end
+    end
 end
