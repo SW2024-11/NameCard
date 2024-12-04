673088afd5a468f408e3e066d691ee575a859c9e
Yui Nemoto <2211140081j@ed.fuk.kindai.ac.jp>
2024-12-04 08:10:53 +0000

    mypage_ver2

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
   get 'top/main'
   post 'top/login'
   
-  root 'top#main'
-  #root 'cards#index'
+  #root 'top#main'
+  root 'cards#index'
   
   resources :users
   resources :cards
   
   get 'get_image/:id', to: 'images#get_image'
+  get 'cards', to: 'cards#index', as: 'card_with_view_type'
 end
