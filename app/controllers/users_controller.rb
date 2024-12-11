52894f9335ffb26ca2701bd0ddcc3fc580009610
Yusuke Kajita <kaji@example.com>
2024-12-11 13:28:47 +0000

    users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,29 +2,26 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
-  def new_form
-  end
 
   def new
     @user = User.new
   end
   
   def create
-    c = User.new(my_company: params[:user][:my_company],
-                 my_name: params[:user][:my_name],
+    c = User.new(my_company: params[:user][:my_company], 
+                 my_name: params[:user][:my_name], 
                  pass: params[:user][:pass])
     c.save
-    redirect_to root_path
+    redirect_to users_path
   end
   
   def destroy
     User.find(params[:id]).destroy
-    redirect_to root_path
+    redirect_to users_path
   end
   
   def show
-    @users = User.find(params[:id])
+    @user = User.find(params[:id])
   end
   
   def edit
@@ -32,10 +29,13 @@ class UsersController < ApplicationController
   end
   
   def update
-    c = User.update(my_company: params[:user][:my_company],
+    @user = User.find(params[:id])
+    if @user.update(my_company: params[:user][:my_company],
                  my_name: params[:user][:my_name],
                  pass: params[:user][:pass])
-    c.save
-    redirect_to root_path
+    redirect_to users_path
+    else
+      render 'edit'
+    end
   end
 end
